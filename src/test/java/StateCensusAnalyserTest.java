484d20204a884d8df59e4de362018affd93c1c01
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-30 04:57:56 +0530

    ADDED: Happy test case to return number of entries when filename is correct

---
+++ src/test/java/StateCensusAnalyserTest.java
@@ -0,0 +1,19 @@
+import com.bridgelabz.censusanalyser.exception.CensusAnalyserException;
+import com.bridgelabz.censusanalyser.service.StateCensusAnalyser;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StateCensusAnalyserTest {
+    private static final String STATE_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenFileName_WhenProper_ShouldReturnTrueCountOfEntries() {
+        try {
+                StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+                int numOfRecords = stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_CSV_FILE_PATH);
+                Assert.assertEquals(29,numOfRecords);
+        } catch (CensusAnalyserException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
